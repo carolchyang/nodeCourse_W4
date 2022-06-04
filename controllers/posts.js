680620fce6b6621525d26de198e4fcd00e7012e8
carolchyang <chingheui.yang@gmail.com>
2022-06-05 06:41:45 +0800

    fix: 訂正助教告知錯誤

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const handleSuccess = require("../service/handleSuccess");
 const handleErrors = require("../service/handleErrors");
 const Post = require("../models/post");
+const User = require("../models/user");
 
 const posts = {
   async getPosts(req, res) {
@@ -86,17 +87,35 @@ const posts = {
      */
 
     try {
-      const { user, image, content } = req.body;
+      const { _id, user, image, content } = req.body;
+      const isExist = User.findById(_id).exe();
 
-      const newPost = await Post.create({
-        user,
-        image,
-        content,
-      });
-      handleSuccess(res, newPost);
+      if (!content.trim() || isExist == null) {
+        handleErrors(res, err);
+      } else {
+        const newPost = await Post.create({
+          user,
+          image,
+          content,
+        });
+        handleSuccess(res, newPost);
+      }
     } catch (err) {
       handleErrors(res, err);
     }
+    /*
+      POST 新增貼文時，建議判斷有沒有這個使用者，如果傳入不存在的 id ( 相似 id 但使用者資料庫中沒有此 id )，也會被新增成功，但 user 會是 null。
+
+      若要確保使用者存在，可以使用 findById 確認是否有使用者 ID 再做 create 的動作
+
+      可以參考 mongoose 文件 的做法 :
+      [Model Name].findById(id).exec() // 如果找不到就是 null
+      或是用 Callback 的方式 :
+
+      [Model Name].findById(id, function(err, userDoc) {
+        // 找到使用者資料就做特定的事情
+      })
+    */
   },
 };
 
